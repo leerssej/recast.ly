81ac03d92c1ead34bf80f377890d83e01508bde0
Hack Reactor Student <student@hackreactor.com>
2018-06-27 17:33:46 -0700

    Change app to stateful functional and first three app tests pass

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,52 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <Search/>
+// var App = () => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <Search/>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <VideoPlayer currentVideo={exampleVideoData[0]}/>
+//       </div>
+//       <div className="col-md-5">
+//         <VideoList videos={exampleVideoData}/>
+//       </div>
+//     </div>
+//   </div>
+// );
+
+// // In the ES6 spec, files are "modules" and do not share a top-level scope
+// // `var` declarations will only exist globally where explicitly defined
+// window.App = App;
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  } // closes constructor
+ 
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search/>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={exampleVideoData[0]}/>
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={exampleVideoData}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer currentVideo={exampleVideoData}/>
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={exampleVideoData}/>
-      </div>
-    </div>
-  </div>
-);
+    );
+  } // closes render method 
+} // closes App class
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
-
-// class App extends ReactComponent {
-//   constructor(props) {
-//     super(props);
-//   } // closes constructor
- 
-//   render() {
-//     return (
-//       <div>
-//         <nav className="navbar">
-//           <div className="col-md-6 offset-md-3">
-//             <Search/>
-//           </div>
-//         </nav>
-//         <div className="row">
-//           <div className="col-md-7">
-//             <VideoPlayer/>
-//           </div>
-//           <div className="col-md-5">
-//             <VideoList videos={exampleVideoData}/>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   } // closes render method 
-
-// } // closes App class
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,11 +1,11 @@
-var VideoPlayer = ({currentVideo}) => (
+var VideoPlayer = ({video}) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/" allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${video.id.videoId}`} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>currentVideo.snippet.title</h3>
-      <div>currentVideo.snippet.description</div>
+      <h3>{video.snippet.title}</h3>
+      <div>{video.snippet.description}</div>
     </div>
   </div>
 );

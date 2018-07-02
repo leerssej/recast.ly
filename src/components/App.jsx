993664b02ebc2177aec490ee656690e880c47659
leerssej <jens.leerssen@gmail.com>
2018-07-02 07:06:35 -0700

    converts App to class App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,33 @@
-var App = ({videos}) => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video={videos[0]} />
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={videos}/>
-      </div>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      videos: exampleVideoData,
+      currentVideo: exampleVideoData[0],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.currentVideo} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videos}/>
+          </div>
+        </div>
+      </div>
+    );
+  } // closes render
+} // closes app
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;

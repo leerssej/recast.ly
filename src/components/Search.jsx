5f7eb9db2e9862c1dbad89da472810bbba5f97ea
leerssej <jens.leerssen@gmail.com>
2018-07-04 16:24:11 -0700

    transforms search component to stateful component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,39 @@
-var Search = ({search}) => (
-  <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button className="btn hidden-sm-down"
-      onClick={() => search($('.form-control').val())}
-    >
-      <span className="glyphicon glyphicon-search"></span>
-    </button>
-  </div> 
-);
+class Search extends React.Component {
+  constructor ({search}) {
+    super({search});
 
+    this.state = {
+      value: '',
+    };
+
+  }
+  
+  setQueryText(queryText) {
+    this.props.search(queryText);
+    this.setState({
+      value: queryText,
+    });
+    
+    console.log('QueryTextSet: ', this);
+  }
+
+  
+  render() {
+    return (
+      <div className="search-bar form-inline">
+        <input 
+          className="form-control"
+          type="text" 
+        />
+        <button className="btn hidden-sm-down"
+          onClick={() => this.setQueryText($('.form-control').val())}
+        >
+          <span className="glyphicon glyphicon-search"></span>
+        </button>
+      </div> 
+    );
+  }
+}
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.Search = Search;
